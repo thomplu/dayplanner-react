49463885bfd4e7cb9872a25176d32d757e43c904
plusklang <thomas.plucinski@yahoo.de>
2025-01-11 02:28:28 +0100

    Fix warnings

--- src/components/CheckBox/Checkbox.tsx
+++ src/components/CheckBox/Checkbox.tsx
@@ -1,24 +1,30 @@
-import "./Checkbox.scss"
+import './Checkbox.scss';
 
 type CheckboxProps = {
-    id: string
-    label: string
-    checked: boolean
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
-}
+    id: string;
+    label: string;
+    checked: boolean;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
 function Checkbox({ id, label, checked, onChange }: CheckboxProps) {
     const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        onChange(event)
-    }
+        onChange(event);
+    };
 
     return (
         <label className="cb">
-            <input id={id} className="cb__input" type="checkbox" checked={checked} onChange={handleOnChange}/>
+            <input
+                id={id}
+                className="cb__input"
+                type="checkbox"
+                checked={checked}
+                onChange={handleOnChange}
+            />
             <span className="cb__checkmark"></span>
-            <span className="cb__label">{ label }</span>
+            <span className="cb__label">{label}</span>
         </label>
     );
-};
+}
 
 export default Checkbox;
--- src/components/RadioBtn/RadioBtn.tsx
+++ src/components/RadioBtn/RadioBtn.tsx
@@ -1,31 +1,38 @@
-import "./RadioBtn.scss"
+import './RadioBtn.scss';
 
 type RadioBtnProps = {
-    id: string
-    label: string
-    options: Record<string, string>
-    value: string
-    checked: boolean
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
-}
+    id: string;
+    label: string;
+    options: Record<string, string>;
+    value: string;
+    checked: boolean;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
 function RadioBtn({ id, label, options, checked, onChange }: RadioBtnProps) {
     const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        onChange(event)
-    }
+        onChange(event);
+    };
 
     return (
         <div className="radio__group">
-            <label>{ label }</label>
+            <label>{label}</label>
             {Object.keys(options).map((key) => (
                 <label className="radio" key={key}>
-                    <input id={key} className="radio__input" name={id} type="radio" checked={checked} onChange={handleOnChange}/>
+                    <input
+                        id={key}
+                        className="radio__input"
+                        name={id}
+                        type="radio"
+                        checked={checked}
+                        onChange={handleOnChange}
+                    />
                     <span className="radio__checkmark"></span>
-                    <span className="radio__label">{ options[key] }</span>
+                    <span className="radio__label">{options[key]}</span>
                 </label>
             ))}
         </div>
     );
-};
+}
 
 export default RadioBtn;
